644fdeaf6e2e9ed40b08556ce70ef1f301aec80c
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-02-24 11:49:42 +0200

    handle AppDetailCell to be self sizing cell

--- AppleAppStore/HelperClasses/Extensions.swift
+++ AppleAppStore/HelperClasses/Extensions.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 extension UILabel {
-    convenience init(text: String, font: UIFont) {
+    convenience init(text: String, font: UIFont, numberOfLines: Int = 1) {
         self.init(frame: .zero)
         self.text = text
         self.font = font
+        self.numberOfLines = numberOfLines
     }
 }
-
 extension UIImageView {
     convenience init(cornerRadius: CGFloat) {
         self.init(image: nil)
--- AppleAppStore/Service/Service.swift
+++ AppleAppStore/Service/Service.swift
@@ -44,8 +44,6 @@ class Service {
     
     func fetchGenericJSONData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> ()) {
         
-        print("T is type:", T.self)
-        
         guard let url = URL(string: urlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
             if let err = err {

c246207f4120bce87edcf5dd0681689c5f65b3b3
Hassan Ramadan <hassanramadan4443@gmail.com>
2022-03-05 18:23:38 +0200

    adding floating controls view to FullAppScreenController with transform animation in scrolling

--- AppleAppStore/Controllers/Today/AppFullscreenController.swift
+++ AppleAppStore/Controllers/Today/AppFullscreenController.swift
@@ -18,6 +18,14 @@ class AppFullscreenController: UIViewController, UITableViewDataSource, UITableV
             scrollView.isScrollEnabled = false
             scrollView.isScrollEnabled = true
         }
+        let height = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        let translationY = -90 - height
+        let transform = scrollView.contentOffset.y > 100 ? CGAffineTransform(translationX: 0, y: translationY) : .identity
+        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            
+            self.floatingContainerView.transform = transform
+        })
     }
     
     let tableView = UITableView(frame: .zero, style: .plain)
@@ -29,6 +37,7 @@ class AppFullscreenController: UIViewController, UITableViewDataSource, UITableV
         
         setupTableView()
         setupCloseButton()
+        setupFloatingControls()
         
     }
     
@@ -56,6 +65,55 @@ class AppFullscreenController: UIViewController, UITableViewDataSource, UITableV
         view.addSubview(closeButton)
         closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 40))
         closeButton.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+    }
+    
+    let floatingContainerView = UIView()
+    
+    @objc fileprivate func handleTap() {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            
+            self.floatingContainerView.transform = .init(translationX: 0, y: -90)
+        })
+    }
+    fileprivate func setupFloatingControls() {
+        floatingContainerView.clipsToBounds = true
+        floatingContainerView.layer.cornerRadius = 16
+        view.addSubview(floatingContainerView)
+        
+        floatingContainerView.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: -90, right: 16), size: .init(width: 0, height: 90))
+        
+        let blurVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        floatingContainerView.addSubview(blurVisualEffectView)
+        blurVisualEffectView.fillSuperview()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        // add our subviews
+        let imageView = UIImageView(cornerRadius: 16)
+        imageView.image = todayItem?.image
+        imageView.constrainHeight(constant: 68)
+        imageView.constrainWidth(constant: 68)
+        
+        let getButton = UIButton(title: "GET")
+        getButton.setTitleColor(.white, for: .normal)
+        getButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        getButton.backgroundColor = .darkGray
+        getButton.layer.cornerRadius = 16
+        getButton.constrainWidth(constant: 80)
+        getButton.constrainHeight(constant: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            VerticalStackView(arrangedSubviews: [
+                UILabel(text: "Life Hack", font: .boldSystemFont(ofSize: 18)),
+                UILabel(text: "Utilizing your Time", font: .systemFont(ofSize: 16))
+            ], spacing: 4),
+            getButton
+        ], customSpacing: 16)
+        
+        floatingContainerView.addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        stackView.alignment = .center
     }
     
     
